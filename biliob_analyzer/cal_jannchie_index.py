e53890791bc9f79d188a63aef342a6c64fae2b32
jannchie <jannchie@gmail.com>
2020-02-29 21:05:10 +0900

    FEATURE: Catch Exception

--- biliob_analyzer/cal_jannchie_index.py
+++ biliob_analyzer/cal_jannchie_index.py
@@ -1,6 +1,7 @@
 from db import db
 import datetime
 from time import sleep
+import logging
 weight = {'coin': 0.4,
           'favorite': 0.3,
           'danmaku': 0.4,
@@ -11,23 +12,26 @@ weight = {'coin': 0.4,
 sum_weight = sum(weight.values())
 count = 0
 for video in db['video'].find().batch_size(100):
-  sleep(0.02)
-  count += 1
-  if count % 10000 == 0:
-    date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-    print("[{}] 计算完毕数量：{}".format(date, count))
-  if 'data' not in video:
-    continue
-  for each_data in video['data']:
-    if 'jannchie' not in each_data:
-      jannchie = 0
-      temp_weight = 0
-      for each_key in weight:
-        if each_key in each_data and each_data[each_key] != None:
-          jannchie += weight[each_key] * each_data[each_key]
-          temp_weight += weight[each_key]
-      if temp_weight < sum_weight:
-        jannchie = jannchie / temp_weight * sum_weight
-      each_data['jannchie'] = int(jannchie)
-  db['video'].save(video)
+  try:
+    sleep(0.02)
+    count += 1
+    if count % 10000 == 0:
+      date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+      print("[{}] 计算完毕数量：{}".format(date, count))
+    if 'data' not in video:
+      continue
+    for each_data in video['data']:
+      if 'jannchie' not in each_data:
+        jannchie = 0
+        temp_weight = 0
+        for each_key in weight:
+          if each_key in each_data and each_data[each_key] != None:
+            jannchie += weight[each_key] * each_data[each_key]
+            temp_weight += weight[each_key]
+        if temp_weight < sum_weight:
+          jannchie = jannchie / temp_weight * sum_weight
+        each_data['jannchie'] = int(jannchie)
+    db['video'].save(video)
+  except Exception as e:
+    logging.exception(e)
   pass
